02c3648784f24859b67cd91ee4abbb4f19ca6454
therealprojectx <xavier.stallings@gmail.com>
2017-06-11 18:38:39 -0400

    added sitecoreextension project for pipelines, renderings, aggregators, etc. extensions.  i also changed the dynamic placeholder provider to fortis

---
+++ src/Foundation/SitecoreExtensions/code/code/ContentEditor/HtmlHelperExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using DynamicPlaceholders.Mvc.Extensions;
+using Sitecore.Mvc.Helpers;
+
+namespace MarketingVillage.Foundation.SitecoreExtensions.ContentEditor
+{
+    public static class HtmlHelperExtensions
+    {
+        public static HtmlString DynamicPlaceholder(this SitecoreHelper helper, string placeholderName)
+        {
+            return SitecoreHelperExtensions.DynamicPlaceholder(helper, placeholderName);
+        }
+
+    }
+}
